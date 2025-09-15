09b5647e23fc7f48487cfb4f1ce464581014854c
Hakonimatata2 <haaardal@stud.ntnu.no>
2025-09-15 10:56:07 +0200

    first commit

---
+++ fist sim/build/CMakeFiles/MySim.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySim.dir/src/sc1/UnderwaterTestApp.cpp.o"
+  "CMakeFiles/MySim.dir/src/sc1/UnderwaterTestApp.cpp.o.d"
+  "CMakeFiles/MySim.dir/src/sc1/UnderwaterTestManager.cpp.o"
+  "CMakeFiles/MySim.dir/src/sc1/UnderwaterTestManager.cpp.o.d"
+  "CMakeFiles/MySim.dir/src/sc1/main.cpp.o"
+  "CMakeFiles/MySim.dir/src/sc1/main.cpp.o.d"
+  "MySim"
+  "MySim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ fist sim/build/CMakeFiles/MySim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MySim.
+# This may be replaced when dependencies are built.
